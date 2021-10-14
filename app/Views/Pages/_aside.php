e632bb3e2ec6ecfb0b9b8d2073ee76e9e83610ca
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-10-14 11:37:39 -0300

    Update _aside.php

--- app/Views/Pages/_aside.php
+++ app/Views/Pages/_aside.php
@@ -14,7 +14,7 @@ if (!isset($subtitle)) { $subtitle = 'DRASHBOARD'; }
 <aside class="sidenav navbar navbar-vertical navbar-expand-xs border-0 border-radius-xl my-3 fixed-start ms-3 " id="sidenav-main">
     <div class="sidenav-header">
       <i class="fas fa-times p-3 cursor-pointer text-secondary opacity-5 position-absolute end-0 top-0 d-none d-xl-none" aria-hidden="true" id="iconSidenav"></i>
-      <a class="navbar-brand m-0 text-center" href="<?php echo base_url(PATH.'res/painel');?>">
+      <a class="navbar-brand m-0 text-center" href="<?php echo base_url(PATH.'/res/painel');?>">
         <!-- <img src="../assets/img/logo-ct.png" class="navbar-brand-img h-100" alt="main_logo"> -->
         <span style="font-family: 'Handel Gothic'; color: #0093DD; font-size: 2.75em; line-height: 100%; padding: 0px;">BRAPCI</span>
         <br/>
@@ -29,7 +29,7 @@ if (!isset($subtitle)) { $subtitle = 'DRASHBOARD'; }
         {
             echo '
                     <li class="nav-item">
-                    <a class="nav-link active" href="'.base_url(PATH.$url).'">
+                    <a class="nav-link active" href="'.base_url(PATH.'/'.$url).'">
                     <div class="icon icon-shape icon-sm shadow border-radius-md bg-white text-center me-2 d-flex align-items-center justify-content-center">
                         <svg width="12px" height="12px" viewBox="0 0 42 42" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink">
                         <title>office</title>
