ea52ceead12b25e733e284a74c2103af8581ed3b
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-10-14 11:41:45 -0300

    Update _head.php

--- app/Views/Pages/_head.php
+++ app/Views/Pages/_head.php
@@ -27,13 +27,13 @@
   <!--     Fonts and icons     -->
   <link href="https://fonts.googleapis.com/css?family=Open+Sans:300,400,600,700" rel="stylesheet" />
   <!-- Nucleo Icons -->
-  <link href="<?php echo base_url('assets/css/nucleo-icons.css');?>" rel="stylesheet" />
-  <link href="<?php echo base_url('assets/css/nucleo-svg.css');?>" rel="stylesheet" />
+  <link href="<?php echo base_url($_SERVER['app.baseURL'].'assets/css/nucleo-icons.css');?>" rel="stylesheet" />
+  <link href="<?php echo base_url($_SERVER['app.baseURL'].'assets/css/nucleo-svg.css');?>" rel="stylesheet" />
   <!-- Font Awesome Icons -->
   <script src="https://kit.fontawesome.com/42d5adcbca.js" crossorigin="anonymous"></script>
-  <link href="../assets/css/nucleo-svg.css" rel="stylesheet" />
+  <link href="<?php echo $_SERVER['app.baseURL']; ?>assets/css/nucleo-svg.css" rel="stylesheet" />
   <!-- CSS Files -->
-  <link id="pagestyle" href="<?php echo base_url('assets/css/soft-ui-dashboard.css?v=1.0.4c');?>" rel="stylesheet" />
+  <link id="pagestyle" href="<?php echo base_url($_SERVER['app.baseURL'].'assets/css/soft-ui-dashboard.css?v=1.0.4c');?>" rel="stylesheet" />
 </head>
 <?php require("_css_fonts.php");?>
 <body class="g-sidenav-show  bg-gray-100">
