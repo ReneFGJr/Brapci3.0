fa411b0bc7ca2162094b71cde42f1f89898d17cc
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-10-15 16:51:23 -0300

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -109,6 +109,26 @@ class Database extends Config
 		'failover' => [],
 		'port'     => 3306,
 	];	
+
+	public $brapci3 = [
+		'DSN'      => '',
+		'hostname' => 'localhost',
+		'username' => '',
+		'password' => '',
+		'database' => '',
+		'DBDriver' => 'MySQLi',
+		'DBPrefix' => '',
+		'pConnect' => false,
+		'DBDebug'  => (ENVIRONMENT !== 'production'),
+		'charset'  => 'utf8',
+		'DBCollat' => 'utf8_general_ci',
+		'swapPre'  => '',
+		'encrypt'  => false,
+		'compress' => false,
+		'strictOn' => false,
+		'failover' => [],
+		'port'     => 3306,
+	];	
 	/**
 	 * This database connection is used when
 	 * running PHPUnit database tests.
