60900d3a3c438fbc5479d68880e3ac07239f4ff7
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-10-16 07:37:05 -0300

    v0.21.10.16

---
+++ app/Controllers/Book.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Controllers;
+
+use App\Controllers\BaseController;
+
+helper(['boostrap','url','graphs','sisdoc_forms','form','nbr']);
+
+define("LIBRARY", "3001");
+define("LIBRARY_NAME", "BRAPCI_RESEARCH");
+define("PATH",$_SERVER['app.baseURL'].$_SERVER['app.sufix']);
+define("MODULE",'book');
+define("URL",$_SERVER['app.baseURL']);
+
+class Book extends BaseController
+{
+		private function cab($tp = '')
+	{
+		$hd = new \App\Models\Header\Header();
+		$tela = '';
+		$dt['title'] = 'Authority';
+		$dt['menu'][''] = 'main';
+		$dt['menu']['index/list'] = 'list';
+		
+		$title = lang(MODULE.'.'.$dt['title']);
+		switch ($tp) {
+			case 'typping':
+				$tela .= $hd->typing($title,lang(MODULE.'.'.$dt['title'].'_sub'));
+				break;
+			case 'footer':
+				$tela .= view('Pages/_footer');
+				break;
+			case 'menu':
+				$tela .= $hd->menu($dt);
+				break;				
+			default:
+				$tela = view('Pages/_head');
+				$tela .= view('Pages/_aside');
+				$tela .= view('Pages/_main_00');
+				$tela .= view('Pages/_navbar');
+				$tela .= view('Pages/_menu_top',$dt);
+
+				break;
+		}
+		return $tela;
+	}
+
+	public function index($d1='',$d2='',$d3='',$d4='')
+	{
+		$Books = new \App\Models\Book\Books();
+
+		$tela = $this->cab('all');
+		$tela .= $Books->index($d1,$d2,$d3,$d4);
+		$tela .= $this->cab('footer');
+
+		return $tela;
+	}
+}
---
+++ app/Models/Book/Books.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace App\Models\Book;
+
+use CodeIgniter\Model;
+
+class Books extends Model
+{
+	protected $DBGroup              = 'default';
+	protected $table                = 'books';
+	protected $primaryKey           = 'id';
+	protected $useAutoIncrement     = true;
+	protected $insertID             = 0;
+	protected $returnType           = 'array';
+	protected $useSoftDeletes       = false;
+	protected $protectFields        = true;
+	protected $allowedFields        = [];
+
+	// Dates
+	protected $useTimestamps        = false;
+	protected $dateFormat           = 'datetime';
+	protected $createdField         = 'created_at';
+	protected $updatedField         = 'updated_at';
+	protected $deletedField         = 'deleted_at';
+
+	// Validation
+	protected $validationRules      = [];
+	protected $validationMessages   = [];
+	protected $skipValidation       = false;
+	protected $cleanValidationRules = true;
+
+	// Callbacks
+	protected $allowCallbacks       = true;
+	protected $beforeInsert         = [];
+	protected $afterInsert          = [];
+	protected $beforeUpdate         = [];
+	protected $afterUpdate          = [];
+	protected $beforeFind           = [];
+	protected $afterFind            = [];
+	protected $beforeDelete         = [];
+	protected $afterDelete          = [];
+
+	function index($d1,$d2,$d3)
+		{
+			$this->path = base_url(PATH.'/index/');
+			$this->path_back = base_url(PATH.'/index/');
+			switch ($d1)
+				{
+					default:
+						$tela = $this->tableview();
+						break;
+				}
+			return $tela;
+		}	
+
+	function tableview()
+		{
+			$tela = '';
+			return $tela;
+		}
+}
