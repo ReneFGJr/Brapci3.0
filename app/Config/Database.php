c2433d44c19741be8b07ed5f0c7889cccbe4569d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-10-20 10:54:12 -0300

    Update Database.php

--- app/Config/Database.php
+++ app/Config/Database.php
@@ -149,7 +149,27 @@ class Database extends Config
 		'strictOn' => false,
 		'failover' => [],
 		'port'     => 3306,
-	];		
+	];	
+	
+	public $brapci = [
+		'DSN'      => '',
+		'hostname' => 'localhost',
+		'username' => '',
+		'password' => '',
+		'database' => '',
+		'DBDriver' => 'MySQLi',
+		'DBPrefix' => '',
+		'pConnect' => false,
+		'DBDebug'  => (ENVIRONMENT !== 'production'),
+		'charset'  => 'utf8',
+		'DBCollat' => 'utf8_general_ci',
+		'swapPre'  => '',
+		'encrypt'  => false,
+		'compress' => false,
+		'strictOn' => false,
+		'failover' => [],
+		'port'     => 3306,
+	];	
 	
 	/**
 	 * This database connection is used when
