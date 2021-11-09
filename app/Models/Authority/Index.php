325e1c6dd24109f1f97309d62838503bd7395c31
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-11-09 08:47:50 -0300

    Update Index.php

--- app/Models/Authority/Index.php
+++ app/Models/Authority/Index.php
@@ -103,7 +103,7 @@ class Index extends Model
 		$tela = '';
 		$Lattes = new \App\Models\Lattes\LattesXML();
 		$tela = $Lattes->xml($d1,$ida);
-		$tela = refresh(PATH.'index/viewid/'.$id);
+		$tela = redireciona(PATH.'index/viewid/'.$id);
 		return $tela;
 	}
 
