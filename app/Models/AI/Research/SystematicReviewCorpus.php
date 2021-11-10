6236b91aa1334149ed797bbaee0091197f59bb8c
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-11-10 12:18:58 -0300

    Update SystematicReviewCorpus.php

--- app/Models/AI/Research/SystematicReviewCorpus.php
+++ app/Models/AI/Research/SystematicReviewCorpus.php
@@ -14,7 +14,14 @@ class SystematicReviewCorpus extends Model
 	protected $returnType           = 'array';
 	protected $useSoftDeletes       = false;
 	protected $protectFields        = true;
-	protected $allowedFields        = [];
+	protected $allowedFields        = [
+		'id_c','c_study',
+		'author','title','journal',
+		'year','volume','number',
+		'pages','doi','issn',
+		'month','note','eprint',
+		'keyword'
+	];
 
 	// Dates
 	protected $useTimestamps        = false;
