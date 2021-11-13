9772e5f4c50f86f9c3afdd9cf88bb769ad2bdd3d
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-11-13 10:05:25 -0300

    Update sisdoc_form_js.php

--- app/Helpers/sisdoc_form_js.php
+++ app/Helpers/sisdoc_form_js.php
@@ -7,6 +7,14 @@ function onclick($url,$x=800,$y=800,$class="")
     $a .= 'NewWindow=window.open(\''.$url.'\',\'newwin\',\'scrollbars=no,resizable=no,width='.$x.',height='.$y.',top=10,left=10\'); ';  
     $a .= 'NewWindow.focus(); void(0); ';
     $a .= '">';
+
+    $a = '<span ';
+    $a .= ' class="'.$class.'" ';
+    $a .= ' onclick = "';
+    $a .= 'NewWindow=window.open(\''.$url.'\',\'newwin\',\'scrollbars=no,resizable=no,width='.$x.',height='.$y.',top=10,left=10\'); ';  
+    $a .= 'NewWindow.focus(); void(0); ';
+    $a .= '" style="cursor: pointer;">';
+
     return $a;
 }
 
