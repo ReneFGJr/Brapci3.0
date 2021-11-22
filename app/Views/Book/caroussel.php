1f90249e08aedfa193488f95380c9b6564818b11
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2021-11-22 08:30:15 -0300

    v0.21.11.22

--- app/Views/Book/caroussel.php
+++ app/Views/Book/caroussel.php
@@ -1,13 +1,13 @@
 <div id="carouselExampleControls" class="carousel slide" data-bs-ride="carousel">
   <div class="carousel-inner">
     <div class="carousel-item active">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="<?php echo URL.'img/banners/books/Slide1.JPG';?>" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="<?php echo URL.'img/banners/books/Slide2.JPG';?>" class="d-block w-100" alt="...">
     </div>
     <div class="carousel-item">
-      <img src="..." class="d-block w-100" alt="...">
+      <img src="<?php echo URL.'img/banners/books/Slide3.JPG';?>" class="d-block w-100" alt="...">
     </div>
   </div>
   <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControls" data-bs-slide="prev">
