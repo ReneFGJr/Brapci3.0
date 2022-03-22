4ae75c569c05b620ff9cb30d2bb0da5c06fc6bdf
Rene Faustino Gabriel Junior <renefgj@yahoo.com.br>
2022-03-22 08:22:14 -0300

    v0.22.03.22

--- app/Models/Dataverse/Solr.php
+++ app/Models/Dataverse/Solr.php
@@ -40,9 +40,42 @@ class Solr extends Model
 	protected $beforeDelete         = [];
 	protected $afterDelete          = [];
 
+	function index($d1,$d2,$d3)
+		{
+			$sx = $d1;
+			switch($d1)
+				{
+					case 'schema':
+						$sx = $this->updateSchema($d2,$d3);
+						break;
+					default:
+						$menu[PATH.MODULE.'dataverse/solr/schema'] = 'dataverse.Solr.Schema';
+						$menu[PATH.MODULE.'dataverse/solr'] = 'dataverse.Solr';
+						$sx .= menu($menu);
+				}
+			return $sx;			
+		}
+
 	function updateSchema()
 		{
 			//$url = 'curl "http://localhost:8080/api/admin/index/solr/schema"';
 			$url = 'http://localhost/api/admin/index/solr/schema';
+			$file = 'D:\Projeto\www\LattesData\_Documentação\PerfilAplicação\schema_dv.xml';
+			$txt = file_get_contents($file);
+			$txt = troca($txt,'<','&lt;');
+			$txt = explode(chr(10),$txt);
+
+			/*************************************************************************/
+			$file = 'D:\Projeto\www\LattesData\_Documentação\PerfilAplicação\schema.xml';
+			$solr = file_get_contents($file);
+			$solr = troca($solr,'<','&lt;');
+			$solr = explode(chr(10),$solr);
+
+			/* SUBS */
+			$tta = '---';
+			$txa = '<!-- Dataverse copyField from http://localhost:8080/api/admin/index/solr/schema -->';
+			$txb = '<!-- End: Dataverse-specific -->';
+
+			pre($txt);
 		}
 }
